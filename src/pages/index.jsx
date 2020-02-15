cf8a5cce5917c2211ea7a5995305e459e2cce332
Lhwhatever <llinhong.2301+github@gmail.com>
2020-02-15 14:40:43 +0800

    Fixes to the index page:
    * Fix so that links show up as intended in 'compact' mode
    * Compact mode changed from xs-sm to xs only.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,8 +18,19 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     alignItems: 'center'
   },
-  btnLink: {
-    marginBottom: theme.spacing(1)
+  btnLinks: {
+    display: 'flex',
+    flexDirection: 'column',
+    '& a': { marginBottom: theme.spacing(1) }
+  },
+  btnLinksCompact: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    '& a': {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1)
+    }
   },
   image: {
     flexGrow: 3
@@ -38,25 +49,35 @@ const LinkSectionHeader = ({ className }) => (
   <Box className={className} m={1}><Typography variant="h5">Shengji Helper</Typography></Box>
 )
 
+const Links = props => {
+  const classes = useStyles()
+
+  return (<Box className={props.compact ? classes.btnLinksCompact : classes.btnLinks}>
+    <ButtonLink variant="outlined" to="/calc">Calculator</ButtonLink>
+    <ButtonLink variant="outlined" to="/planner">Game Planner</ButtonLink>
+  </Box>)
+}
+
+
 const IndexPage = () => {
   const classes = useStyles()
 
   return (<Container className={classes.container}>
     <SEO title="Home" />
-    <Hidden smDown>
+    <Hidden xsDown>
       <Box className={classes.root}>
         <Box className={classes.linkCol}>
           <LinkSectionHeader className={classes.linksectheaderbox} />
-          <ButtonLink className={classes.btnLink} variant="outlined" to="/calc">Calculator</ButtonLink>
-          <ButtonLink className={classes.btnLink} variant="outlined" to="/planner">Game Planner</ButtonLink>
+          <Links />
         </Box>
         <Box className={classes.image}><Image /></Box>
       </Box>
     </Hidden>
-    <Hidden mdUp>
+    <Hidden smUp>
       <Box className={classes.smroot}>
         <Box><Image /></Box>
         <LinkSectionHeader className={classes.linksectheaderbox} />
+        <Links compact />
       </Box>
     </Hidden>
   </Container>)
