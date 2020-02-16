eef9eb37582514477479780222c39c5ee0407267
Lhwhatever <llinhong.2301+github@gmail.com>
2020-02-16 18:10:39 +0800

    NumOfPlayerField and SelectField now forward refs

--- src/components/inputs/numOfPlayerField.jsx
+++ src/components/inputs/numOfPlayerField.jsx
@@ -4,12 +4,13 @@ import { MenuItem } from '@material-ui/core'
 import range from '../../helper/range'
 import SelectField from './selectField'
 
-const NumOfPlayerField = props => (
+const NumOfPlayerField = React.forwardRef((props, ref) => (
     <SelectField
         fullWidth
         label={"No. of Players"}
+        ref={ref}
         {...props}
     >{range(4, 11).map(e => <MenuItem key={e} value={e}>{e}</MenuItem>)}</SelectField>
-)
+))
 
 export default NumOfPlayerField
--- src/components/inputs/selectField.jsx
+++ src/components/inputs/selectField.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { TextField } from '@material-ui/core'
 
-const SelectField = ({ children, ...props }) => (
+const SelectField = React.forwardRef(({ children, ...props }, ref) => (
     <TextField
         select
         SelectProps={{ native: props.native ? true : false }}
+        ref={ref}
         {...props}
     >{children}
     </TextField>
-)
+))
 
 export default SelectField
